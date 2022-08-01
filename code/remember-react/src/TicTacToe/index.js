36a9046916d1d9d1b1e4355220cba3d70f60aa40
Daniela Valero <hola@danielavalero.com>
2022-08-01 20:27:52 +0200

    add comment to further tasks

--- code/remember-react/src/TicTacToe/index.js
+++ code/remember-react/src/TicTacToe/index.js
@@ -3,12 +3,12 @@ import "./styles.css";
 
 /**
  * From: https://reactjs.org/tutorial/tutorial.html#setup-option-1-write-code-in-the-browser
- * Display the location for each move in the format (col, row) in the move history list.
- * Bold the currently selected item in the move list.
- * Rewrite Board to use two loops to make the squares instead of hardcoding them.
- * Add a toggle button that lets you sort the moves in either ascending or descending order.
- * When someone wins, highlight the three squares that caused the win.
- * When no one wins, display a message about the result being a draw.
+ * - Display the location for each move in the format (col, row) in the move history list.
+ * - Bold the currently selected item in the move list.
+ * - Rewrite Board to use two loops to make the squares instead of hardcoding them.
+ * - Add a toggle button that lets you sort the moves in either ascending or descending order.
+ * - When someone wins, highlight the three squares that caused the win.
+ * - When no one wins, display a message about the result being a draw.
  */
 function calculateWinner(squares) {
   const lines = [
